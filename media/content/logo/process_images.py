5dfebef7da57064f2e638bcb2f251d8bb454a590
putteneersjoris <putteneersjoris@gmail.com>
2022-03-23 20:34:54 +0100

    update python

--- media/content/logo/process_images.py
+++ media/content/logo/process_images.py
@@ -1,19 +1,38 @@
 
-from math import floor
+from email import header
 import subprocess
 import random
-import math
+import os
 
 
-# subprocess.run("magick mogrify -resize 30x30! *.jpg")
-# subprocess.run("magick convert *.jpg -colorspace gray -fill blue -tint 80  ./pp/%d.jpg")
+
+# make new folder for post processed files
+dir_p = "header_processed"
+pwd = os.getcwd()
+path_p = os.path.join(pwd, dir_p)+ "\\"
 
 
-# subprocess.run("magick mogrify -resize 30x30! %d.jpg")
-subprocess.run("magick convert -resize 30x30! *.jpg ./pp/%d.jpg")
 
-for i in range(6):
+# grab images from original header 
+dir = "header"
+path = os.path.join(pwd, dir) + "\\"
+print (path_p )
+
+# if not already exist, make it otherwie ignore it
+if os.path.isdir(path_p) == False:
+    os.mkdir(path_p)
+    print ('y')
+
+
+subprocess.run("magick convert -resize 30^> {}*.jpg {}%d.jpg".format(path, path_p))
+
+os.chdir(path_p)
+n_files = len(os.listdir(path_p))
+
+for i in range(n_files):
 
     tint = int(random.random() * 100)
-    
-    subprocess.run("magick convert -resize 30x30! ./pp/{}.jpg -colorspace gray *.jpg".format(i))
+    subprocess.run("magick convert {}.jpg   -colorspace gray -fill rgba(80,80,255,1) -tint {} -ordered-dither o8x8 {}.jpg".format(i,tint,i))
+
+# if __name__=="__main__":
+#     process()
