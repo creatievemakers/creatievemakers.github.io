64a7b46acec72fe54b1ca634f60bb8f2031c45e2
putteneersjoris <putteneersjoris@gmail.com>
2022-03-23 18:39:47 +0100

    update logo js array

--- media/content/logo/process_images.py
+++ media/content/logo/process_images.py
@@ -1,8 +1,19 @@
 
+from math import floor
 import subprocess
+import random
+import math
 
 
-subprocess.run("magick mogrify -resize 30x30! *.jpg")
-subprocess.run("magick convert *.jpg -colorspace gray -fill blue -tint 100 -ordered-dither o8x8 -set filename:base \"%[basename]\" \"%[filename:base].jpg\"")
+# subprocess.run("magick mogrify -resize 30x30! *.jpg")
+# subprocess.run("magick convert *.jpg -colorspace gray -fill blue -tint 80  ./pp/%d.jpg")
 
 
+# subprocess.run("magick mogrify -resize 30x30! %d.jpg")
+subprocess.run("magick convert -resize 30x30! *.jpg ./pp/%d.jpg")
+
+for i in range(6):
+
+    tint = int(random.random() * 100)
+    
+    subprocess.run("magick convert -resize 30x30! ./pp/{}.jpg -colorspace gray *.jpg".format(i))
--- scripts/script.js
+++ scripts/script.js
@@ -30,10 +30,10 @@ fetch("site/"+clicked_id+".html")
 
     // logo functionality (wip)
 
-    let images = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','12.jpg','13.jpg','14.jpg','1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','12.jpg','13.jpg','14.jpg','1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','12.jpg','13.jpg','14.jpg','1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','12.jpg','13.jpg','14.jpg','1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','12.jpg','13.jpg','14.jpg','1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','12.jpg','13.jpg','14.jpg','1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','12.jpg','13.jpg','14.jpg','1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','12.jpg','13.jpg','14.jpg','1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','12.jpg','13.jpg','14.jpg'];
+    let images = ['0.jpg','1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','12.jpg','13.jpg','14.jpg','1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','12.jpg','13.jpg','14.jpg','1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','12.jpg','13.jpg','14.jpg','1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','12.jpg','13.jpg','14.jpg','1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','12.jpg','13.jpg','14.jpg','1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','12.jpg','13.jpg','14.jpg','1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','12.jpg','13.jpg','14.jpg','1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','12.jpg','13.jpg','14.jpg','1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','12.jpg','13.jpg','14.jpg'];
 
     const delayTime = 20
-    let length = Math.min(images.length , 20)
+    let length = Math.min(images.length , 13)
     
     function add_function(){
             adding(0);
@@ -48,9 +48,9 @@ fetch("site/"+clicked_id+".html")
         setTimeout(function() {
             adding(i + 1)
 
-            array.push("<img src = \"../media/content/logo/"  + images[Math.floor(Math.random()*length)] + "\">")
+            array.push("<img src = \"../media/content/logo/pp/"  + images[Math.floor(Math.random()*length)] + "\">")
             
-            document.getElementById("f").innerHTML = array + "projects"
+            document.getElementById("f").innerHTML = array
 
                 // anti-glitch
                 if(Object.keys(array).length > length){
